37d681e98f5501229416717161e85999170c8c19
Maikol <maikolsoro.z1998@gmail.com>
2022-11-25 16:34:15 -0800

    Class Generic, Pattern Repository and UnitOfWork

---
+++ POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // Declaracion o matricula de nuestra interfaces a nivel repository
+
+        ICategoryRepository Category { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
